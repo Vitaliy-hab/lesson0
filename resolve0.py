9344d4324fa00a253b4b1655700461a9059d39f4
Vitaliy-hab <vetaliln030@gmail.com>
2024-06-26 02:14:22 +0700

    Add files via upload

---
+++ resolve0.py
@@ -0,0 +1,28 @@
+a = ("good morning")
+print(len(a))
+a = [1, 2, 3, 5, 8, 9]
+print(len(a))
+a = ["good morning"]
+print(len(a))
+
+first = 12
+second = 6
+summa = (first + second)
+diff = (first - second)
+print("summa",summa, "diff",diff)
+
+first = 3
+second = 6
+third = 8
+mean = ((first + second + third) / 3)
+print("mean",mean)
+
+first_string = ("Понедельник")
+second_string = ("Вторник")
+print(first_string,", ",second_string)
+
+a = 4
+b = 6
+c = 8
+f = ((((a*b)+(a*c))**3)//2)
+print(f)
